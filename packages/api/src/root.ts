089db5259c9dcfc2ec6cb18e4c114553971cab57
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-07-25 15:25:59 +0300

    ✨ (root.ts): introduce new routers for category and folder to enhance API functionality and structure

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,4 +1,6 @@
 import { authRouter } from "./router/auth";
+import { categoryRouter } from "./router/category";
+import { folderRouter } from "./router/folder";
 import { postRouter } from "./router/post";
 import { reminderRouter } from "./router/reminder";
 import { createTRPCRouter } from "./trpc";
@@ -7,6 +9,8 @@ export const appRouter = createTRPCRouter({
   auth: authRouter,
   post: postRouter,
   reminder: reminderRouter,
+  folder: folderRouter,
+  category: categoryRouter,
 });
 
 // export type definition of API
