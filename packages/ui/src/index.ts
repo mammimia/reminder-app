b25d631b73189c90b6f1b59836a809dabb953cb6
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-07-28 18:34:53 +0300

    ⬆️ (ui): upgrade lucide-react dependency to version 0.417.0 to add new icons support in the UI package.

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,8 @@
 import { cx } from "class-variance-authority";
 import { twMerge } from "tailwind-merge";
 
+export * from "lucide-react";
+
 const cn = (...inputs: Parameters<typeof cx>) => twMerge(cx(inputs));
 
 export { cn };
