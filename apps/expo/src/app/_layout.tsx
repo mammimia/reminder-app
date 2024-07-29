ccd0fc5fa1bd2cc04b2d737ed0ddac022cda3d28
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-07-29 16:48:47 +0300

    📝 (expo/src/app/_layout.tsx): Add back button to the header for navigation functionality in the app ✨ (expo/src/app/_layout.tsx): Introduce createBottomTabNavigator for improved navigation experience in the app

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,34 +1,71 @@
 import "@bacons/text-decoder/install";
 
-import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useColorScheme } from "nativewind";
 
 import { TRPCProvider } from "~/utils/api";
 
 import "../styles.css";
 
+import { Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import Accounting from "./accounting";
+import DailyRoutine from "./daily-routine";
+import Reminder from "./reminder";
+
 // This is the main layout of the app
 // It wraps your pages with the providers they need
+
+const Tab = createBottomTabNavigator();
+
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
+  const { goBack, canGoBack } = useNavigation();
   return (
     <TRPCProvider>
       {/*
-          The Stack component displays the current page.
-          It also allows you to configure your screens 
+      Add back button to the header
         */}
-      <Stack
+      <StatusBar />
+      <Tab.Navigator
         screenOptions={{
           headerStyle: {
-            backgroundColor: "#f472b6",
+            backgroundColor: "#f4511e",
           },
-          contentStyle: {
-            backgroundColor: colorScheme == "dark" ? "#09090B" : "#FFFFFF",
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerLeft: () => {
+            if (!canGoBack()) {
+              return null;
+            }
+
+            return (
+              <TouchableOpacity
+                style={{
+                  alignItems: "center",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  padding: 10,
+                }}
+                onPress={goBack}
+              >
+                <Text>
+                  <Ionicons name="arrow-back" size={18} color="white" />
+                </Text>
+              </TouchableOpacity>
+            );
           },
         }}
-      />
-      <StatusBar />
+      >
+        <Tab.Screen name="reminder/index" component={Reminder} />
+        <Tab.Screen name="daily-routine/index" component={DailyRoutine} />
+        <Tab.Screen name="accounting/index" component={Accounting} />
+      </Tab.Navigator>
     </TRPCProvider>
   );
 }
